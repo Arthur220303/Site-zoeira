4573799f11dcf9b3b8c156c8c0afba6070527f29
Arthur220303 <av490477@gmail.com>
2026-02-27 21:59:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 function enviar(){
 
-let msg = document.getElementById("msg").value;
+let msgInput = document.getElementById("msg");
+let msg = msgInput.value;
+
 if(msg.trim() === "") return;
 
 let chat = document.getElementById("chat");
 
-chat.innerHTML += `<p>${msg}</p>`;
+/* comandos */
+if(msg.startsWith("/")){
+msg = executarComando(msg);
+chat.innerHTML += `<p class="system">${msg}</p>`;
+}
+else{
+chat.innerHTML += `<p class="user">${msg}</p>`;
+}
 
-document.getElementById("msg").value="";
+msgInput.value="";
 chat.scrollTop = chat.scrollHeight;
+}
+
+/* comandos zoeira */
+function executarComando(cmd){
+
+switch(cmd){
+
+case "/help":
+return "📜 Comandos: /help /oi /fig";
+
+case "/oi":
+return "😈 Oi, tudo bem?";
+
+case "/fig":
+return "🖼️ Figura ativada!";
+
+default:
+return "❌ Comando não encontrado";
+}
 
 }
